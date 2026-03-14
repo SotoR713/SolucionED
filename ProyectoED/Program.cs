6f917861170bd132dd981a60028cf85aca1f2edf
JesusSoto <Soto_713@hotmail.com>
2026-03-14 14:00:27 -0600

    Ejercicios con arreglos y ciclos

--- ProyectoED/Program.cs
+++ ProyectoED/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] nombres = new string[5];
+            nombres[0] = "Juan";
+            nombres[1] = "María";
+            nombres[2] = "Pedro";
+            nombres[3] = "Ana";
+            nombres[4] = "Luis";
+            //nombres[5] = "Carlos"; // Esto causará un error de índice fuera de rango
+
+            // Imprimir el primer nombre
+            //            Console.WriteLine(nombres[0]);
+
+            //imprimir el segundo nombre utilizando una variable
+            //          string segundoNombre = nombres[1];
+            //           Console.WriteLine(segundoNombre);
+
+            // imprimit todos los nombres utilizando un bucle for each
+            //foreach (string nombre in nombres)
+            //{
+            //    Console.WriteLine(nombre);
+            //}
+
+            //imprimir todos los nombres utilizando un bucle for
+            //for (int i = 0; i < nombres.Length; i++)
+            //{
+            //    Console.WriteLine(nombres[i]);
+            //}
+
+            ////imprimir todos los nombres utilizando while
+            //int i = 0;
+            //while (i < nombres.Length)
+            //{
+            //    Console.WriteLine(nombres[i]);
+            //    i++;
+            //}
+
+            ////imprimir todos los nombres utilizando do while
+            //int i= 0;
+            //do 
+            //{
+            //    Console.WriteLine(nombres[i]);
+            //    i++;
+            //} while (i < nombres.Length);
         }
     }
 }
